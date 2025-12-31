d1ee7e429d165ed99f6c307b6edbd42b24043afd
Bipin Bhatt <bipinbhatt280@gmail.com>
2025-12-31 00:51:17 -0500

    chore: setup database connection

---
+++ src/config/dbConfig.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import {DB_URL}  from "./serverConfig.js";
+
+export default async function connectDB() {
+    try {
+        await mongoose.connect(DB_URL)
+        console.log("Connected to mongoDB")
+    } catch (error) {
+        console.log("Something went wrong while connecting to mongoDB. ")
+        console.log(error)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import connectDB from './config/dbConfig.js'
 const app = express() // this app is a server object instance
 const PORT = 3000
 
@@ -11,12 +12,8 @@ app.get('/ping',(req,res)=>{
         message:"Pong"
     })
 })
-app.get('/hello',(req,res)=>{
-    res.json({
-        message:"hello world!"
-    })
-})
 
 app.listen(PORT,()=>{
     console.log(`server is listening on port ${PORT}`)
+    connectDB()
 })
