dd6f02f10277fe36ff9e6cfb1b9605833f9a9944
Bipin Bhatt <bipinbhatt280@gmail.com>
2026-01-17 01:02:36 -0500

    fix(validate): pass ZodError to global handler instead of ApiError and optimize middleware

--- src/validators/zodValidator.js
+++ src/validators/zodValidator.js
@@ -4,16 +4,10 @@ import ApiError from "../utils/apiErrorHandler.js"
 export const validate = (schema)=>{
     return (req,res,next)=>{
         try {
-            //check if body is empty 
-            if(!req.body || Object.keys(req.body).length === 0){
-                next(new ApiError(400,"Please provide all the information."))
-            }
-            
-            schema.parse(req.body)
+            schema.parse(req.body||{})
             next()
         } catch (e) {
-            console.log(e)
-            next(new ApiError(400,e.issues[0].message||"Validation Error"))
+            next(e)
         
         }
     }
