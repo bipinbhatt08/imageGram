d720a2c142c8be9760123d4ca55fb08e2618c2cc
Bipin Bhatt <bipinbhatt280@gmail.com>
2026-02-03 00:48:31 -0500

    fix(like): ensure a user can like a Post or Comment only once by adding unique compound index

--- src/schema/like.js
+++ src/schema/like.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import user from "./user";
 const likeSchema = mongoose.Schema({
 
 //polymorphic Like model so a single collection can handle likes for both posts and comments
@@ -21,6 +22,13 @@ const likeSchema = mongoose.Schema({
 
 
 },{timestamps: true})
+
+//unique compound index to prevent duplicate like.
+likeSchema.index({
+    user:1,
+    likeableId:1,
+    onModel:1
+},{unique:true})
 
 const Like = mongoose.model("Like",likeSchema)
 export default Like
